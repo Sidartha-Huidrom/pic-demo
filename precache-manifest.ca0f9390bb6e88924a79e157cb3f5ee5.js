e34cf5822ff58c1c8d4a1d527c352806d9048fc3
Sidartha-Huidrom <sidarth.buckethead@gmail.com>
2020-08-20 22:05:01 +0530

    Updates

---
+++ precache-manifest.ca0f9390bb6e88924a79e157cb3f5ee5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fafe728e3bbda6d6ea55b8a8fb06a3a5",
+    "url": "/pic-demo/index.html"
+  },
+  {
+    "revision": "77fb5c9b06a8af5ba885",
+    "url": "/pic-demo/static/css/main.4bdc3e1c.chunk.css"
+  },
+  {
+    "revision": "1ce7a8600996f1842855",
+    "url": "/pic-demo/static/js/2.c48dffeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pic-demo/static/js/2.c48dffeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77fb5c9b06a8af5ba885",
+    "url": "/pic-demo/static/js/main.3d2b2715.chunk.js"
+  },
+  {
+    "revision": "daf2676a000cb24b50ff",
+    "url": "/pic-demo/static/js/runtime-main.df7ee99a.js"
+  }
+]);
